929676bd15a46d0548addad514c15cd59fe60855
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-28 13:48:36 +0300

    Add error in TaskForm and task_form.html if tags not exist

--- todo_list/forms.py
+++ todo_list/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from todo_list.models import Task, Tag
 
@@ -6,12 +7,18 @@ from todo_list.models import Task, Tag
 class TaskForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.CheckboxSelectMultiple
     )
 
     class Meta:
         model = Task
         fields = "__all__"
+
+    def clean_tags(self):
+        tags = self.cleaned_data.get("tags")
+        if not tags:
+            raise ValidationError("Please select at least one tag.")
+        return tags
 
 
 class TagForm(forms.ModelForm):
